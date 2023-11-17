3652ff18d3a80335843aebf10c358ead933ac4a7
kahlahA <31360043+kahlahA@users.noreply.github.com>
2023-11-17 15:09:43 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+// UserSignUp.js
+import React, { useState } from 'react';
+import './App.css'; // Import the CSS file
+
+function UserSignUp() {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    username: '',
+    email: '',
+    address: '',
+    phoneNumber: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log('User Data:', formData);
+    // You can further process or submit the data here
+  };
+
+  return (
+    <form>
+      <label className='userName'>
+        User Full Name:
+        <input type="text" name="fullName" onChange={handleChange} />
+      </label>
+      <br />
+
+      <label className='desiredName'>
+        User's desired username:
+        <input type="text" name="username" onChange={handleChange} />
+      </label>
+      <br />
+
+      <label className='email'>
+        User email:
+        <input type="email" name="email" onChange={handleChange} />
+      </label>
+      <br />
+
+      <label className='address'>
+        User Address:
+        <input type="text" name="address" onChange={handleChange} />
+      </label>
+      <br />
+
+      <label className='phoneNumber'>
+        User Phone Number:
+        <input type="tel" name="phoneNumber" onChange={handleChange} />
+      </label>
+      <br />
+
+      <button  type="button" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default UserSignUp;
